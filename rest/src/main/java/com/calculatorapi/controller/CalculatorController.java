5fd963bd67c9a45f7d03800b4b1cf764eec6426d
Bruno <bruno.freitas.cardoso@hotmail.com>
2025-03-03 18:47:05 +0000

    Implementerest api logging with request ID

--- rest/src/main/java/com/calculatorapi/controller/CalculatorController.java
+++ rest/src/main/java/com/calculatorapi/controller/CalculatorController.java
@@ -1,5 +1,9 @@
 package com.calculatorapi.controller;
 
+import com.calculatorapi.filter.UniqueIdentifierFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import com.calculatorapi.service.CalculatorService;
 
@@ -10,25 +14,33 @@ import java.math.RoundingMode;
 @RequestMapping("/api")
 public class CalculatorController {
 
-    private final CalculatorService calculatorService = new CalculatorService();
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     @GetMapping("/add")
-    public BigDecimal add(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public BigDecimal add(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletRequest request) {
+        String requestId = (String) request.getAttribute(UniqueIdentifierFilter.REQUEST_ID_HEADER);
+        logger.info("Summation request: a = {}, b = {}", a, b);
         return a.add(b);
     }
 
     @GetMapping("/subtract")
-    public BigDecimal subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public BigDecimal subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletRequest request) {
+        String requestId = (String) request.getAttribute(UniqueIdentifierFilter.REQUEST_ID_HEADER);
+        logger.info("Subtraction request: a = {}, b = {}", a, b);
         return a.subtract(b);
     }
 
     @GetMapping("/multiply")
-    public BigDecimal multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public BigDecimal multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletRequest request) {
+        String requestId = (String) request.getAttribute(UniqueIdentifierFilter.REQUEST_ID_HEADER);
+        logger.info("Multiplication request: a = {}, b = {}", a, b);
         return a.multiply(b);
     }
 
     @GetMapping("/divide")
-    public BigDecimal divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public BigDecimal divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletRequest request) {
+        String requestId = (String) request.getAttribute(UniqueIdentifierFilter.REQUEST_ID_HEADER);
+        logger.info("Division request: a = {}, b = {}", a, b);
         if (b.equals(BigDecimal.ZERO))
             throw new ArithmeticException();
         return a.divide(b, RoundingMode.HALF_UP);
