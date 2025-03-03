34a0715c381c49706860d9252482182fda946768
Bruno <bruno.freitas.cardoso@hotmail.com>
2025-03-03 18:55:43 +0000

    Implement calculator service with basic operations

---
+++ calculator/src/main/java/com/calculatorapi/CalculatorApplication.java
@@ -0,0 +1,17 @@
+package com.calculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
